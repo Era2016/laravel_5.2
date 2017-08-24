f0d9d114a744222e2ecf96915c5f83633a9fab11
fan <zhangjiyuan-rj@ofo.com>
2017-08-24 17:23:22 +0800

    init env

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,12 +41,12 @@ Route::get('test', 'HomeController@index');*/
     'password' => 'Auth\PasswordController'
 ]);*/
 
-/*Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/', function () {
-        redirect('/');
+        return view('welcome');
     });
     Route::get('/home', 'HomeController@index');
-});*/
+});
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
